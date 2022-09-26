2cc89a671b3ad4821aa492f3e3a9b44c106c654b
David Ruiz Diaz <david@debian-BULLSEYE-live-builder-AMD64>
2022-09-25 20:22:14 -0400

    init

---
+++ js/application.js
@@ -0,0 +1,22 @@
+/*
+	Planeta		Gravedad
+	Mercurio	3,7 m/s²
+	Venus		8,87 m/s²
+	La Tierra	9,8 m/s²
+	Marte		3,71 m/s²
+	Júpiter		24,79 m/s²
+	Saturno		10,44 m/s²
+	Urano		8,87 m/s²
+	Neptuno		11,15 m/s²
+	Pluton		0.62 m/s²
+
+
+							Formula (regla de 3 simple)
+
+							pesoTierra --- gravedadTierra
+									    \
+				 (pesoOtroPlaneta = x) --- gravedadOtroPlaneta
+
+		pesoOtroPlaneta = (pesoTierra * gravedadOtroPlaneta) / gravedadTierra
+*/
+
